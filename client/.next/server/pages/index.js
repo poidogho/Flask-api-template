fd8b6bbd97df2e20e52097ef1d6ca406fcdd6da8
poidogho <poidogho@yahoo.com>
2022-01-15 18:04:20 -0500

    nextjs

---
+++ client/.next/server/pages/index.js
@@ -0,0 +1,42 @@
+"use strict";
+(() => {
+var exports = {};
+exports.id = 405;
+exports.ids = [405];
+exports.modules = {
+
+/***/ 656:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "default": () => (/* binding */ pages)
+});
+
+;// CONCATENATED MODULE: external "react/jsx-runtime"
+const jsx_runtime_namespaceObject = require("react/jsx-runtime");
+;// CONCATENATED MODULE: ./pages/index.js
+
+/* harmony default export */ const pages = (()=>{
+    /*#__PURE__*/ jsx_runtime_namespaceObject.jsx("p", {
+        children: "Next JS"
+    });
+});
+
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(656));
+module.exports = __webpack_exports__;
+
+})();
---
+++ client/pages/index.js
@@ -0,0 +1,3 @@
+export default () => {
+  <p>Next JS</p>;
+};
