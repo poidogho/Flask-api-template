0589ed6dba25ba170160625ccd76464764034b6a
poidogho <poidogho@yahoo.com>
2022-01-15 21:28:59 -0500

    initial setup

--- client/.next/static/XjEviSDLzjgPnKWkzB-0U/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2e48cc7322e610d4.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- client/pages/index.js
+++
@@ -1,3 +0,0 @@
-export default () => {
-  <p>Next JS</p>;
-};
